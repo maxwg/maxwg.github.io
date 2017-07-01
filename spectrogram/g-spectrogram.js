100d2b0bd7aa6c23080e903388dcdbae6d5707fb
maxwg <maxwang@hotmail.com.au>
2017-07-01 23:15:37 +1000

    Update g-spectrogram.js

--- spectrogram/g-spectrogram.js
+++ spectrogram/g-spectrogram.js
@@ -233,7 +233,10 @@ var nyquist = context.sampleRate/2;
     var percent = value / 255;
     var delta = percent * (toH - fromH);
     var hue = fromH + delta;
-    return 'hsl(H, 100%, L%)'.replace(/H/g, hue).replace(/L/g, Math.max(30,100-percent*100));
+    if (percent < 0.03){
+      return '#222'; 
+    }
+    return 'hsl(H, 100%, 50%)'.replace(/H/g, hue);
   },
   
   logChanged: function() {
