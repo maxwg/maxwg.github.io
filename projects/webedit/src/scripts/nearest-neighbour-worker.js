9bb7f5331ae806154e6d9da99db7339cf0da537e
maxwg <_mw@outlook.com>
2017-07-02 02:22:56 +1000

    switched to submodules

--- projects/webedit/src/scripts/nearest-neighbour-worker.js
+++
@@ -1,7 +0,0 @@
-﻿
-self.onmessage = function (e) {
-    importScripts( "hadamard-transform.js", "image-helpers.js", "kdtree.js", "nearest-neighbour.js", "nearest-neighbour-stats.js");
-    var yuv = ImageHelpers.RGBtoYUV(e.data.img);
-    var offsets = ANNStats(8)(yuv);
-    self.postMessage({ "return": offsets });
-};
